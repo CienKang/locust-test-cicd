af5ff318e430957f444bd37afa5291341eb5765a
Mehak Noor Singh <68504159+CienKang@users.noreply.github.com>
2024-09-30 18:59:42 +0530

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -2,22 +2,22 @@ from locust import HttpUser, TaskSet, task, between
 import random
 
 class UserTasks(TaskSet):
-    @task(1,name="Get all Users")
+    @task(1)
     def get_all_users(self):
-        self.client.get("/users")
+        self.client.get("/users",name="Get all Users")
 
     @task(2)
     def get_user(self, name="Get some Users"):
         user_id = random.randint(1, 2)  # Assuming we have 2 users with IDs 1 and 2
-        self.client.get(f"/users/{user_id}")
+        self.client.get(f"/users/{user_id}",name="Get a user")
 
-    @task(3,name="Create a new user")
+    @task(3)
     def create_user(self):
         new_user = {
             "name": f"User {random.randint(3, 100)}",
             "email": f"user{random.randint(1, 100)}@example.com"
         }
-        self.client.post("/users", json=new_user)
+        self.client.post("/users", json=new_user,name="Create a user ")
 
     @task(4)
     def update_user(self, name="Update a user"):
@@ -26,7 +26,7 @@ class UserTasks(TaskSet):
             "name": f"Updated User {random.randint(1, 100)}",
             "email": f"updated_user{random.randint(1, 100)}@example.com"
         }
-        self.client.put(f"/users/{user_id}", json=updated_user)
+        self.client.put(f"/users/{user_id}", json=updated_user,name="Update a user")
 
 class WebsiteUser(HttpUser):
     tasks = [UserTasks]
